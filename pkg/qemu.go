34e641cc120250467cf86c48c947111bdf129d76
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-14 17:07:24 +0200

    detect accel type at runtime

--- pkg/qemu.go
+++ pkg/qemu.go
@@ -2,12 +2,17 @@ package pkg
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func getQemuArgs(vars Variables) [][]string {
+	accel := "kvm"
+	if runtime.GOOS == "darwin" {
+		accel = "hvf"
+	}
 	return [][]string{
 		{"-m", fmt.Sprintf("%s", vars.Memory)},
-		{"-machine", "accel=hvf"},
+		{"-machine", "accel=" + accel},
 		{"-cpu", "max"},
 		{"-smp", fmt.Sprintf("cpus=%s", vars.Cpus)},
 	}
