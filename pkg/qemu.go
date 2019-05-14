d1ff5520cc4f3c56ef8606537fd0ded61b6611aa
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-14 17:40:28 +0200

    fix kvm detection

--- pkg/qemu.go
+++ pkg/qemu.go
@@ -2,17 +2,22 @@ package pkg
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 )
 
 func getQemuArgs(vars Variables) [][]string {
-	accel := "kvm"
+	machine := []string{"-machine", "pc"}
 	if runtime.GOOS == "darwin" {
-		accel = "hvf"
+		machine = []string{"-machine", "accel=hvf"}
+	}
+
+	if _, err := os.Stat("/dev/kvm"); err != nil {
+		machine = []string{"-machine", "accel=kvm"}
 	}
 	return [][]string{
 		{"-m", fmt.Sprintf("%s", vars.Memory)},
-		{"-machine", "accel=" + accel},
+		machine,
 		{"-cpu", "max"},
 		{"-smp", fmt.Sprintf("cpus=%s", vars.Cpus)},
 	}
